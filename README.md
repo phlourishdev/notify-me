fc17111677d274307279aef7810ba6e88aaef4fb
kuehnol <verk@posteo.org>
2024-05-27 22:32:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# notify-me
+
+Short bash script to notify me to take the fries out of the oven. So far only tested on Fedora Linux and Gnome. It uses `aplay` and `notify-send`. If it doesn't work on your machine, feel free to open an issue or pull request.
+
+## Usage
+
+1. Allow execution of the script
+```bash
+chmod +x ./notify-me.sh
+```
+
+2. Execute it 
+```bash
+./notify-me.sh $TIME $TIMER_DESC
+```
+The format of `$TIME` is HH:MM:SS, so `"1:30:30"`, for 1 hour, 30 minutes and 30 seconds.
+`$TIMER_DESC` can be left empty.
+
+## Attributions
+
+New Notification #7 by UNIVERSFIELD -- https://freesound.org/s/736267/ -- License: Attribution 4.0
---
+++ notify-me.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+{
+    # get arguments
+    whole_timer_string=$1 # timer value in HH:MM:SS
+    timer_description=$2 # description for the timer
+
+    # seperate string into three time parts
+    IFS=":" read -ra time_parts <<< "$whole_timer_string"
+
+    hours="${time_parts[0]}"
+    minutes="${time_parts[1]}"
+    seconds="${time_parts[2]}"
+
+    # check element count
+    element_count=${#time_parts[@]}
+
+    if [ $element_count != 3 ]; then
+        echo "Invalid format: Use HH:MM:SS."
+        exit 1
+    fi
+
+    # check for empty elements in array
+    for ((i=0; i<${#time_parts[@]}; i++)); do
+        # check if the current element is empty
+        if [[ -z ${time_parts[i]} ]]; then
+            echo "Invalid format: Element at index $i is empty. Use HH:MM:SS."
+            exit 1
+        fi
+
+        # check if current element is not an integer
+        if ! [[ ${time_parts[i]} =~ ^[0-9]+$ ]]; then
+            echo "Invalid format: Element at index $i is not an integer."
+            exit 1
+        fi
+    done
+
+    # check if minutes and seconds have valid values
+    if [ $minutes -gt 59 ] || [ $seconds -gt 59 ]; then
+        echo "Invalid value: Value for minutes and/or seconds is greater than 59."
+        exit 1
+    fi
+
+    if [ $hours -lt 0 ] || [ $minutes -lt 0 ] || [ $seconds -lt 0 ]; then
+        echo "Invalid value: Value for hours, minutes and/or seconds is less than 0."
+        exit 1
+    fi
+
+    # calculate the amount of total seconds
+    total_seconds=$(($seconds + 60 * $minutes + 3600 * $hours))
+
+    # wait the amount of total seconds
+    for ((i=$total_seconds; i > 0; i--)); do
+        sleep 1
+    done
+
+    # send a notification
+    notify-send "Timer has ended" "$timer_description"
+
+    # play notificaton sound
+    aplay ./assets/notification.wav > /dev/null 2>&1 # mute output
+
+    # exit
+    exit 0
+} &
